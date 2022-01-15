e52ece9bb2ecd37aa045cc1700db382610d757e8
DiamondYuan <admin@diamondyuan.com>
2022-01-16 02:08:09 +0800

    fix: hack eslint plugin

--- src/bin/eslint/eslint.ts
+++ src/bin/eslint/eslint.ts
@@ -1,10 +1,18 @@
 const Module = require('module');
 const _require = Module.prototype.require;
 const set = new Set();
+const path = require('path');
+
+const _resolve = Module._resolveFilename;
+
+// eslint-disable-next-line
 Module.prototype.require = function reallyNeedRequire(name: string) {
   if (!set.has(name) && !name.startsWith('.')) {
-    console.log('require', name);
+    // console.log('require', name);
     set.add(name);
+    if (name.endsWith('fake-typescript-eslint-eslint-plugin/index.js')) {
+      return _require.call(this, path.join(__dirname, 'typescript-eslint-plugin.js'));
+    }
   }
   return _require.call(this, name);
 };
